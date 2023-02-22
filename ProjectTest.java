e51fbc89b9ae1d4f5604e84b17e6fcf8afef6926
John Doe <jack.h.sawyer@pm.me>
2023-02-21 16:51:54 -0800

    Revisited portfolio class

--- ProjectTest.java
+++ ProjectTest.java
@@ -2,9 +2,11 @@ class ProjectTest {
     public static void main(String[] args) {
 
         Project project = new Project();
+        Portfolio portfolio = new Portfolio();
 
         String pitch = project.elevatorPitch("Jack Sawyer",  "Full Stack Java Developer", "$92K per annum");
 
-        System.out.println(pitch); 
+        System.out.println(pitch);
+        System.out.println(Portfolio);
     }
 }
